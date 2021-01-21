a39fa74a09c465b99def039df285da2ed864dd79
elsa-k-donovan <36769721+elsa-k-donovan@users.noreply.github.com>
2021-01-21 11:45:27 -0500

    Create Arrays-Left-Rotation.py

---
+++ Arrays/Arrays-Left-Rotation.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Additional method to move Left
+def moveLeft(a):
+    first = a[0]
+    a.pop(0)
+    a.append(first)
+    return a
+        
+
+# Complete the rotLeft function below.
+def rotLeft(a, d):
+    for x in range(0,d):
+        a = moveLeft(a)
+    return a
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nd = input().split()
+
+    n = int(nd[0])
+
+    d = int(nd[1])
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = rotLeft(a, d)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
